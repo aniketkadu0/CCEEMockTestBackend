69a5b0a7d8ede445fbddb236d94967832af30d87
aniketkadu0 <aniketkadu@gmail.com>
2023-02-04 15:08:33 +0530

    logger change

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ morgan.token('id', function getId (req) {
     return req.id
   })
 
-
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(morgan(':date :id :method :url :status :response-time'));
+app.use(morgan(':date :id :method :url :status :response-time :remote-addr'));
+// app.use(morgan('dev'))
 
 app.use("/user", userRoute);
 
