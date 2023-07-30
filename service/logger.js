f3da15b562330953455cf5bd87bca10568016ada
aniketkadu0 <aniketkadu@gmail.com>
2023-07-30 17:04:29 +0530

    add try catch to axios

--- service/logger.js
+++ service/logger.js
@@ -3,5 +3,7 @@ const axios = require("axios");
 exports.addlog = async (log) => {
   const BASE_URL = "https://gentle-boot-bass.cyclic.app";
   const PATH = "/addlog";
-  return axios.post(BASE_URL + PATH, log);
+  try {
+    axios.post(BASE_URL + PATH, log);
+  } catch (err) {}
 };
