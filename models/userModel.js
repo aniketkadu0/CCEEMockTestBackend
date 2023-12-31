900f69266bb16588cd4466ed64aeabd6bc620662
aniketkadu0 <aniketkadu@gmail.com>
2023-12-31 19:48:55 +0530

    expired field added in userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,46 +1,50 @@
 const { bool } = require("joi");
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
-    {
-        firstName:{
-            type: String,
-            trim: true,
-            required:true,
-        },
-        lastName:{
-            type: String,
-            trim:true,
-            required:true,
-        },
-        centreName:{
-            type: String,
-            required: true,
-        },
-        email:{
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-        },
-        password:{
-            type: String,
-            required: true,
-        },
-        role:{
-            type: String,
-            default : 'user'
-        },
-        verified:{
-            type: Boolean,
-            default : true
-        },
-        paymentStatus:{
-            type: Boolean,
-            default : false
-        },
-        tests : [{}]
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    {timestamps: true}
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    centreName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    verified: {
+      type: Boolean,
+      default: true,
+    },
+    paymentStatus: {
+      type: Boolean,
+      default: false,
+    },
+    tests: [{}],
+    expired: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
